a4734be9c8b963a094d636e5d2a43c0971047e21
HaryMani <73690901+HaryMani@users.noreply.github.com>
2021-03-08 11:03:15 +0400

    Update actions.py
    on comment custom action code

--- actions/actions.py
+++ actions/actions.py
@@ -6,34 +6,34 @@
 
 
 # This is a simple example for a custom action which utters "Hello World!"
-import requests
-from typing import Any, Text, Dict, List
-from rasa_sdk import Action, Tracker
-from rasa_sdk.executor import CollectingDispatcher
-from rasa_sdk.events import SlotSet
+#import requests
+#from typing import Any, Text, Dict, List
+#from rasa_sdk import Action, Tracker
+#from rasa_sdk.executor import CollectingDispatcher
+#from rasa_sdk.events import SlotSet
 
-class ActionSetUserName(Action):
-    def name(self) -> Text:
-        return "set_username"
+#class ActionSetUserName(Action):
+#    def name(self) -> Text:
+#        return "set_username"
 
-    def run(self, dispatcher: CollectingDispatcher,
-            tracker: Tracker,
-            domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
-        usernm = tracker.get_slot("username")
-        msg = "Nice to talk to you "
-        dispatcher.utter_message(text="Hello {}:{}".format(usernm, msg))
+#    def run(self, dispatcher: CollectingDispatcher,
+#            tracker: Tracker,
+#            domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+#        usernm = tracker.get_slot("username")
+#        msg = "Nice to talk to you "
+#        dispatcher.utter_message(text="Hello {}:{}".format(usernm, msg))
 
-        return [SlotSet(("username", usernm))]
+#        return [SlotSet(("username", usernm))]
 
-class ActionSetNewsLetter(Action):
-    def mail(self) -> Text:
-        return "set_newsletter"
+#class ActionSetNewsLetter(Action):
+#    def mail(self) -> Text:
+#        return "set_newsletter"
 
-    def run(self, dispatcher: CollectingDispatcher,
-            tracker: Tracker,
-            domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
-        usermel = tracker.get_slot("usermail")
-        msg = " has been subscribed to newsletter successfully "
-        dispatcher.utter_message(text="Great, {}:{}".format(usermel, msg))
+#    def run(self, dispatcher: CollectingDispatcher,
+#            tracker: Tracker,
+#            domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+#        usermel = tracker.get_slot("usermail")
+#        msg = " has been subscribed to newsletter successfully "
+#        dispatcher.utter_message(text="Great, {}:{}".format(usermel, msg))
 
-        return [SlotSet(("usermail", usermel))]
+#        return [SlotSet(("usermail", usermel))]
